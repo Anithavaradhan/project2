d537441064cb9d841f87011000aeea1d40511bd4
Anitha <anithaa357@gmail.com>
2020-02-26 22:20:55 +1300

    Added javaproject

---
+++ Automation2/src/javaprogram/Javaprogram.java
@@ -0,0 +1,24 @@
+package javaprogram;
+
+public class Javaprogram {
+
+	
+	
+		void ifcondition() {
+			int age=20;
+			if (age>=18)
+			System.out.println("eligible to vote");
+			else
+			System.out.println("not eligible to vote");
+			}
+
+			public static void main(String []args) {
+				
+				Javaprogram a1=new Javaprogram();
+			a1.ifcondition();
+
+			}
+			}
+	
+
+
